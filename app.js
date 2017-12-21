ac43d8af98f5aa76e4871c3996fb077c16fec812
Max Soe <maxsoe@gmail.com>
2017-12-21 23:29:44 +0000

    06 - services (without factories)

--- app.js
+++ app.js
@@ -1,21 +1,16 @@
-var app = angular.module('firstAngularApp',[]);
+var app = angular.module('firstAngularApp', []);
 
-app.controller('mainCtrl', function($http) {
+app.controller('mainCtrl', function(mainSvc) {
   var vm = this;
   this.hello = "world";
-
   this.fruits = ['apple', 'orange', 'grape'];
-
-  $http.get('https://jsonplaceholder.typicode.com/posts')
-    .then(function(response) {
-      vm.posts = response.data;
-  })
-
-
-
   this.alertMe = function() {
     alert('Button pressed');
   };
+  mainSvc.getPosts()
+    .then(function(response) {
+      vm.posts = response.data;
+    })
 });
 
 app.filter('makePlural', function() {
@@ -23,3 +18,9 @@ app.filter('makePlural', function() {
     return (input + "s");
   };
 });
+
+app.service('mainSvc', function($http) {
+  this.getPosts = function() {
+    return $http.get('https://jsonplaceholder.typicode.com/posts');
+  }
+})
